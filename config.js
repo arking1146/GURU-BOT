6d742afe50b574a47500890eb21ed7f66cd0ff6d
arking1146 <166977481+arking1146@users.noreply.github.com>
2024-04-14 17:35:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+923137811619', 'GURU', true],
+  ['+923460784775', 'Ash', true], 
   [''] 
 ] //Number of owners
 
